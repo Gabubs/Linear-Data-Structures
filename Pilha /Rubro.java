1ce8881f895671dc0cb39de9170f7dd7e76b7849
Gabriel G. Santos <Gabrielgs2uwu@gmail.com>
2025-10-14 02:29:27 +0000

    Pilha

---
+++ Pilha /Rubro.java
@@ -0,0 +1,116 @@
+import java.util.Arrays;
+public class Rubro {
+    // Vermelha Cresce
+    // Preta Diminui
+    private int capacity;
+    private int sizeV;
+    private int sizeP; // Capacidade - Qtd itens na pilha preta
+    private Object[] lista;
+
+    public Rubro() {
+        this.capacity = 1;
+        this.sizeP = this.capacity;
+        this.sizeV = 0;
+        this.lista = new Object[this.capacity];
+    }
+
+    private void increaseCapacity() {
+        int newCapacity = this.capacity*2;
+        int newSizeP = newCapacity - realSizeP();
+        Object[] newList = new Object[newCapacity];
+
+        for (int i = 0; i < this.sizeV; i++)
+            newList[i] = this.lista[i];
+        for (int i = 0; i < realSizeP(); i++)
+            newList[newSizeP + i] = this.lista[this.sizeP + i];
+
+        this.sizeP = newSizeP;
+        this.capacity = newCapacity;
+        this.lista = newList;
+    }
+    private void decreaseCapacity() {
+        int newCapacity = this.capacity/2;
+        int newSizeP = newCapacity - realSizeP();
+        Object[] newList = new Object[newCapacity];
+        if (newCapacity < 1)
+            newCapacity = 1;
+
+        for (int i = 0; i < this.sizeV; i++)
+            newList[i] = this.lista[i];
+        for (int i = 0; i < realSizeP(); i++)
+            newList[newSizeP + i] = this.lista[this.sizeP + i];
+
+        this.sizeP = newSizeP;
+        this.capacity = newCapacity;
+        this.lista = newList;
+    }
+
+
+    private int realSizeP() {
+        return this.capacity - this.sizeP;
+    }
+
+    private int totalSize() {
+        return realSizeP() + this.sizeV;
+    }
+
+    public void pushV(Object value) {
+        if (sizeV >= sizeP)
+            increaseCapacity();
+        this.lista[sizeV++] = value;
+    }
+    public void pushP(Object value) {
+        if (sizeP <= sizeV)
+            increaseCapacity();
+        this.lista[--sizeP] = value;
+    }
+    public boolean isEmptyV() {
+        return this.sizeV == 0;
+    }
+    public boolean isEmptyP() {
+        return realSizeP() == 0;
+    }
+    public Object topP() {
+        return lista[sizeP];
+    }
+    public Object topV() {
+        return lista[sizeV-1];
+    }
+
+    public Object popV() {
+        if (this.sizeV == 0)
+            throw new RuntimeException("Pilha vermelha vazia :( ");
+        // ACHO que é para aparecer o valor que vai ser deletado entao
+        Object o = lista[sizeV-1];
+        if (totalSize() <= capacity/3)
+            decreaseCapacity();
+        this.lista[sizeV-1] = null;
+        this.sizeV--;
+        return o;
+    }
+    public Object popP() {
+        if (this.sizeP == capacity)
+            throw new RuntimeException("Pilha preta vazia :( ");
+        // ACHO que é para aparecer o valor que vai ser deletado entao
+        Object o = lista[sizeP];
+        if (totalSize() <= capacity/3)
+            decreaseCapacity();
+            
+        this.lista[sizeP] = null;
+        this.sizeP++;
+        return o;
+    }
+
+    private int getSizeP () {
+        return realSizeP();
+    }
+    private int getSizeV () {
+        return sizeV;
+    }
+    public void arrayCheck () {
+        System.out.println(Arrays.toString(lista));
+    }
+    public int getCapacity() {
+        return capacity;
+    }
+}
--- README.md
+++ README.md
@@ -1,6 +1,17 @@
 # Linear-Data-Structures
 > here you can find my LDS material
 
+# Pilha
+Rubro - A Stack structure that uses a single array to manage two stacks. Elements ending with “V” correspond to the Vermelho Stack, while those ending with “P” correspond to the Preto Stack.
+ - Vermelho - Left to Right
+ - Preto - Right to Left
+
+<br>
+
+
 # Deque
+No_Simples - Deck structure created with singly linked list  
+No_Duplo - Deck structure created with doubly linked list
+
 > No_Simples - Deck structure created with singly linked list  
 > No_Duplo - Deck structure created with doubly linked list
